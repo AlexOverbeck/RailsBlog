09b89335eb02848fe9a2fea0b74dc5e58fc4df60
Charlie Roth <roth47@purdue.edu>
2016-07-05 23:51:14 -0400

    Clean up code, put articles list into partial.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -21,15 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
     if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to article_path(@article)
@@ -39,16 +35,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-
     @article.destroy
     flash[:notice] = "Article was deleted"
     redirect_to articles_path
   end
 
   private
+
     def set_article
       @article = Article.find(params[:id])
     end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
