5b209a4f0b0413f9aa421bfe0405c61a516ecdf6
Charlie Roth <roth47@purdue.edu>
2016-07-06 22:14:08 -0400

    Add associtaion between user and article.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  has_many :articles
+
+  before_save { self.email = email.downcase }
+
   validates :username,
     presence: true,
     uniqueness: { case_sensitive: false },
@@ -10,4 +15,5 @@ class User < ActiveRecord::Base
     uniqueness: { case_sensitive: false },
     length: { maximum: 100 },
     format: {with: VALID_EMAIL_REGEX}
+
 end
