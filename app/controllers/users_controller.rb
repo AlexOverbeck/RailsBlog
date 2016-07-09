eec6dde8ee48a6c14aadb68053dd2d09112294d0
Charlie Roth <roth47@purdue.edu>
2016-07-09 19:45:36 -0400

    Show articles by user when on profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path
+      redirect_to articles_path
     else
       render :new
     end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
     @user = User.find(params[:id])
   end
 
