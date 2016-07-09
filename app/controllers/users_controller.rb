42f124b338a3161024909a6214cf16b8c7bcc660
Charlie Roth <roth47@purdue.edu>
2016-07-09 19:16:23 -0400

    Add update user, move update/create user form into partial.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-     redirect_to user_path
+      redirect_to user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
