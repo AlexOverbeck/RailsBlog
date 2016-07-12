62e6ddc9eb8ccfb356a4804dab33211efd3ad52e
Charlie Roth <roth47@purdue.edu>
2016-07-12 01:47:31 -0400

    Add routes for login and signup, create flash alerts partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-    root "welcome#home"
-    get 'signup' => "users#new"
+    root 'welcome#home'
+    get 'signup' => 'users#new'
+
     get 'login' => 'sessions#new'
+    post 'login' => 'sessions#create'
+    delete 'logout' => 'sessions#destroy'
+
     resources :users
     resources :articles
 end
