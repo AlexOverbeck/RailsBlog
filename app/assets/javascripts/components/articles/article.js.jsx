08b41a862ff4f8330bee7159dae5e69205fde5a8
Alex Overbeck <alexoverbeck@gmail.com>
2016-07-17 17:31:43 -0400

    use rails link attributes to remove articles with ajax

--- app/assets/javascripts/components/articles/article.js.jsx
+++ app/assets/javascripts/components/articles/article.js.jsx
@@ -1,21 +1,22 @@
 var Article = React.createClass({
   removeArticle: function(event) {
     ArticleActions.removeArticle(this.props.id);
-    event.preventDefault();
   },
 
   render: function() {
     return (
-      <div className="col s12 m4 l4">
-        <div className="card small">
-          <div className="card-content">
-            <span className="card-title">{this.props.title}</span>
+      <div className='col s12 m4 l4'>
+        <div className='card small'>
+          <div className='card-content'>
+            <span className='card-title'>{this.props.title}</span>
             <p>{this.props.description}</p>
           </div>
-          <div className="card-action">
-            <a href='#'>Edit</a>
-            <a href='#'>Show</a>
-            <a href="#" onClick={this.removeArticle}>Delete</a>
+          <div className='card-action'>
+            <a href={'/articles/'+this.props.id}>Show</a>
+            <a href={'/articles/'+this.props.id+'/edit'}>Edit</a>
+            <a href={'/articles/'+this.props.id} onClick={this.removeArticle} data-method='delete' data-remote='true'>
+              Delete
+            </a>
           </div>
         </div>
       </div>
